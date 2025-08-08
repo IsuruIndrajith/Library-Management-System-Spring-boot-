8b53c1bf8f5ba0d4d12f5acb2a00269bd49df4a5
Isuru <isuruindrajith680@gmail.com>
2025-08-08 18:52:45 +0530

    Books table

--- src/main/java/com/example/LibraryManagementSystem/entity/books.java
+++ src/main/java/com/example/LibraryManagementSystem/entity/books.java
@@ -1,4 +1,26 @@
 package com.example.LibraryManagementSystem.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "Books")
 public class books {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //here the primary key is auto generated when a new row is inserted
+    private int book_id;
+    private String isbn;
+    private String title;
+    private String author;
+    private String publisher;
+    private int year_published;
+    private String genre;
+    private int copies_total;
+    private int copies_available;
+
 }
