9dc0b20d4e0976526804bb21acf3663ab91286ad
Isuru <isuruindrajith680@gmail.com>
2025-08-16 08:19:10 +0530

    ant Frontend initialization

---
+++ frontend/frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'antd/dist/reset.css';
+import { } from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
