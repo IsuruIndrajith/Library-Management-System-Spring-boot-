a6853d660187b52809625a1a4746bb8cb059f2ee
Isuru <isuruindrajith680@gmail.com>
2025-08-26 08:00:43 +0530

    DashBoard Graph

--- frontend/frontend/src/API/index.js
+++ frontend/frontend/src/API/index.js
@@ -2,3 +2,8 @@ export const getRecentlyAddedBooks = () => {
    return fetch('https://dummyjson.com/products')
       .then(res => res.json());
 }
+
+export const getNewlyAddedBooks = () => {
+   return fetch('https://dummyjson.com/carts')
+      .then(res => res.json());
+}
